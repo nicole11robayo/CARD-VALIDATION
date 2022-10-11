23e3f2e759b6788e921aea4b66e5e2b82714745f
NICOLE ROBAYO <nicolerobayor@gmail.com>
2022-10-11 16:50:00 -0500

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,111 @@
+document.getElementById("validacion").style.display="none";
+document.getElementById("infoTarjeta").style.display="none";
+document.getElementById("regresar").style.display="none";
+
+document.getElementById("caja").focus();
+
+const b4=document.getElementById("siguiente");
+b4.addEventListener("click",next);
+
+function next(){
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("validacion").style.display="";
+    document.getElementById("mensaje").style.display="none"
+    document.getElementById("caja").focus();
+}
+
+const b1= document.getElementById("ayuda");
+b1.addEventListener("click",help);
+
+function help() {
+    alert("Es el número ubicado en la parte frontal de tu tarjeta");
+}
+
+const b3= document.getElementById("regresar");
+b3.addEventListener("click",back);
+
+function back() {
+    document.location.reload();
+}
+
+/*
+let hh=document.getElementById("caja");
+hh.addEventListener("keyup", (e)=>{
+    console.log(e.target.value)
+    let valorinput= e.target.value;
+    hh.value=valorinput
+    .replace(/\s/g, "")
+    .replace(/([0-9]{4})/g, "$1 ")
+    .trim();
+}) 
+*/
+
+const b2= document.getElementById("validar");
+
+b2.addEventListener("click",function(){
+    let cardNumber= document.getElementById("caja").value
+    if(cardNumber==""){
+        alert("Ingrese un número");
+        document.location.reload();
+    }else if(validator.isValid(cardNumber)==true){
+        document.getElementById("result").innerHTML="El número de tu tarjeta es válido";
+        document.getElementById("validez").innerHTML= "Validez: Válido";
+        document.getElementById("final").innerHTML="Ya puedes empezar a disfrutar de nuestros servicios!"
+    }else if(validator.isValid(cardNumber)==false){
+        document.getElementById("result").innerHTML="El número de tu tarjeta es inválido";
+        document.getElementById("validez").innerHTML= "Validez: Inválido";
+        document.getElementById("final").innerHTML="UPS! regresa y verifica que ingresaste bien tu número"
+    }
+}
+);
+
+b2.addEventListener("click", function(){
+    document.getElementById("infoTarjeta").style.display="";
+    document.getElementById("ingreso").style.display="none";
+    document.getElementById("regresar").style.display="";
+    const cardNumber= document.getElementById("caja").value;
+    
+    document.getElementById("tarCifrada").innerHTML= "Número: "+ validator.maskify(cardNumber);
+    
+}
+);
+
+
+b2.addEventListener("click", marca);
+
+function marca(){
+    let cardNumber= document.getElementById("caja").value;
+    
+    let four= cardNumber.slice(0,4);
+    let three= cardNumber.slice(0,3);
+    let two= cardNumber.slice(0,2);
+    let one= cardNumber.slice(0,1);
+
+    if(four=="2131"||four=="1800"||two=="31"||two=="32"||two=="33"||two=="35"||two=="39"){
+        document.getElementById("franquicia").innerHTML= "Franquicia: JCB";
+    }else if(four == "2014" || four == "2149"){
+        document.getElementById("franquicia").innerHTML= "Franquicia: Diner`s Club / enRoute";
+    }else if(four=="6011"){
+        document.getElementById("franquicia").innerHTML= "Franquicia: Discover";
+    }else if(three=="300"||three=="301"||three=="302"||three=="303"||three=="304"||three=="305"){
+        document.getElementById("franquicia").innerHTML= "Franquicia: Diner`s Club / Carte Blanche";
+    }else if(two=="51"||two=="52"||two=="53"||two=="54"||two=="55"){
+        document.getElementById("franquicia").innerHTML= "Franquicia: Mastercard";
+    }else if(two=="37"||two=="34"){
+        document.getElementById("franquicia").innerHTML= "Franquicia: American Express";
+    }else if(two=="36"){
+        document.getElementById("franquicia").innerHTML= "Franquicia: Diner`s Club / International";
+    }else if(one=="4"){
+        document.getElementById("franquicia").innerHTML= "Franquicia: Visa";
+    }else{
+        document.getElementById("franquicia").innerHTML= "Franquicia: No identificado";
+    }
+
+}
+
+
+
+
 import validator from './validator.js';
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,104 @@
 const validator = {
-  // ...
+    isValid: function(creditNumber){
+  
+        //separar en digitos el numero y ponerlo al reves(string)(array)
+        let sepRev= creditNumber.split('').reverse();
+
+        //recorrer el array para convetir los digitos tipo string a number 
+        const tar=[]
+        for (let i=0; i<sepRev.length; i++){
+            tar.push(parseInt(sepRev[i]))    
+        }
+        
+        //crear 2 arrays nuevos para separar los numeros de indices pares e impares
+        const par=[];
+        const impar=[];
+        for(let ind=0; ind<tar.length; ind++){
+            if(ind%2==0){
+                par.push(tar[ind])
+            }else if(ind%2 != 0) {
+                impar.push(tar[ind])
+            }
+        }
+
+        //nueva variable para guardar en un array los numeros de indices impares multiplicados por 2
+        const impar2=[];
+        for(let m=0; m<impar.length; m++){
+            impar2.push(impar[m]*2)
+        }
+
+        //2 nuevos arrays para separar los numeros de una y dos cifras 
+        const cifras1=[];
+        const cifras2=[];
+
+        for(let d=0;d<impar2.length;d++){
+            if(impar2[d]>9){
+                //los de 2 cifras convertirlos de una en strings para que sea más facil separlos por digitos
+                cifras2.push(impar2[d].toString())
+            }else if(impar2[d]<=9){
+                cifras1.push(impar2[d])
+            }
+        }
+
+        //nuevo array con los numeros de 2 cifras separados por digitos(string)
+        const dig=[];
+        for (let c=0; c<cifras2.length;c++){
+            //con esto cada par de digitos separados van a quedar en arrays distintos anidados dentro de "dig"
+            //ej: dig=[["1","2"],["1","0"],["1","4"]]
+            dig.push(cifras2[c].split(''));        
+        }
+        
+        //for...of para unir en un solo array los arrays anidados que contienen los numeros de 2 cifras separados por digitos
+        //quedaría; dig=["1","2","1","0","1","4"]
+        let unir=[];
+        for (let mini of dig){
+            unir=unir.concat(mini)
+        }
+
+        //convertir a tipo number los elementos del array anterior
+        const impar5=unir.map(Number)
+
+        //unir todos los arrays para hacer la suma final
+        //impar5: array con los numeros de 2 cifras separados en digitos(posicion impar)
+        //cifras1: array con los numeros de 1 cifra (posicion impar)
+        //par: array con los numeros de las posiciones pares
+        const todo= impar5.concat(par,cifras1)
+
+        //iniciar una variable en cero para recorrer el array con todos los elementos a sumar y sumarlos a esa variable
+        let suma=0
+        for(let s=0; s<todo.length; s++){
+            suma += todo[s]
+        }
+
+        //console.log(suma)
+
+    
+        if(suma%10 ==0){
+            return true;
+            
+        }else if(suma%10 !=0){
+            return false;
+        }
+
+    },
+
+    maskify: function(creditNumber){
+        let cifrador="";
+
+        for(let i=0; i<creditNumber.length;i++){
+
+            if(i<creditNumber.length-4){
+                cifrador=cifrador + "#";
+            }else{
+                cifrador=cifrador + creditNumber[i];
+            }
+        }
+        return cifrador
+
+    }
+
+
+ 
 };
 
 export default validator;
